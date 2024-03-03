db9e4753f6080908f56dc3ab4846cf75746fd00b
4rMyy <56570563+starmkodinw@users.noreply.github.com>
2024-03-03 13:25:20 +0700

    Revert "Fix"
    This reverts commit 38a1f967242a3501bda08f3015c30d32b96300b0.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
 import "./styles/kg-card.css";
-// import Head from "next/head";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,20 +12,20 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
-  // title: "hodleveryday.com - เว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร บทความเกี่ยวกับคริปโตเคอร์เรนซี่ Bitcoin และ Blockchain เหมาะสำหรับนักลงทุนทุกระดับ",
-  // description: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
+  title: "hodleveryday.com - เว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร บทความเกี่ยวกับคริปโตเคอร์เรนซี่ Bitcoin และ Blockchain เหมาะสำหรับนักลงทุนทุกระดับ",
+  description: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
   keywords: ["hodleveryday.com", "bitcoin", "BTC", "cryptocurrency", "คริปโตเคอร์เรนซี่", "เงินดิจิทัล", "บิทคอยน์", "บล็อกเชน", "blockchain"],
   applicationName: "hodleveryday",
   authors: [{ name: "hodleveryday", url: "https://hodleveryday.com" }],
   creator: "hodleveryday",
-  // icons: [{ rel: "icon", url: "https://hodleveryday.com/pixel - logo.png" }, { rel: "apple-touch-icon", url: "https://hodleveryday.com/pixel - logo.png" }],
-  // openGraph: {
-    // type: "article",
-    // url: "https://hodleveryday.com",
-    // title: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
-    // description: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
-    // siteName: "hodleveryday",
-    // locale: "th_TH",
+  icons: [{ rel: "icon", url: "https://hodleveryday.com/pixel - logo.png" }, { rel: "apple-touch-icon", url: "https://hodleveryday.com/pixel - logo.png" }],
+  openGraph: {
+    type: "article",
+    url: "https://hodleveryday.com",
+    title: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
+    description: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
+    siteName: "hodleveryday",
+    locale: "th_TH",
     // images: [
     //   {
     //     url: "https://hodleveryday.com/pixel - logo.png",
@@ -35,12 +35,12 @@ export const metadata: Metadata = {
     //     type: "image/png",
     //   }
     // ]
-  // },
-  // twitter: {
-    // card: "summary_large_image",
-    // site: "@hodleveryday",
-    // title: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
-    // description: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@hodleveryday",
+    title: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
+    description: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
     // images: [
     //   {
     //     url: "https://hodleveryday.com/pixel - logo.png",
@@ -50,7 +50,7 @@ export const metadata: Metadata = {
     //     type: "image/png",
     //   }
     // ]
-  // },
+  },
   abstract: "hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ",
   verification: {
     google: "uEacq0IeLU8EBD7DPEeF_4xEd8LslvoJ-4omouVZSkc",
@@ -64,7 +64,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="canonical" href="https://hodleveryday.com/" />
         <meta name="description" content="hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ" />
         <meta property="og:url" content="https://hodleveryday.com" key="ogurl" />
@@ -72,7 +72,7 @@ export default function RootLayout({
         <meta property="og:site_name" content="hodleveryday.com" key="ogsitename" />
         <meta property="og:title" content="hodleveryday.com - เว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร บทความเกี่ยวกับคริปโตเคอร์เรนซี่ Bitcoin และ Blockchain เหมาะสำหรับนักลงทุนทุกระดับ" key="ogtitle" />
         <meta property="og:description" content="hodleveryday.com เป็นเว็บไซต์ที่รวบรวมข้อมูล ข่าวสาร และบทวิเคราะห์เกี่ยวกับคริปโตเคอร์เรนซี่ เหมาะสำหรับนักลงทุนทุกระดับ ไม่ว่าจะเป็นมือใหม่หรือมืออาชีพ" key="ogdesc" />
-      </head>
+      </Head>
       <body className={inter.className}>
         {children}
       </body>
