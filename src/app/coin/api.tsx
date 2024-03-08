a4037a765ceb38f72f4c59071f587e156a426825
4rMyy <56570563+starmkodinw@users.noreply.github.com>
2024-03-08 11:19:39 +0700

    Test dynamic rendering

--- src/app/coin/api.tsx
+++ src/app/coin/api.tsx
@@ -1,6 +1,8 @@
+import { unstable_noStore as noStore } from 'next/cache';
 const COIN = process.env.COIN;
 
 export async function getLastQuote(symbol: string) {
+    noStore();
     const response = await fetch(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${symbol}`, {
         method: 'GET',
         headers: { 'X-CMC_PRO_API_KEY': `${COIN}` },
@@ -11,6 +13,7 @@ export async function getLastQuote(symbol: string) {
 }
 
 export async function getSymbolImage(symbol: string) {
+    noStore();
     const response = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=${symbol}`, {
         method: 'GET',
         headers: { 'X-CMC_PRO_API_KEY': `${COIN}` },
